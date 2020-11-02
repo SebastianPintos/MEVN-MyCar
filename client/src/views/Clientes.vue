3f0e9342d7db661b325e125aa58a5219ea8dfecd
solpierozzi <solpierozzi@hotmail.com>
2020-11-01 23:42:04 -0300

    Modificados nombres, agregados snackbars

--- client/src/views/Clientes.vue
+++ client/src/views/Clientes.vue
@@ -1,360 +1,290 @@
 <template>
-  <v-container>
+<v-container>
     <h1 class="titulo">Clientes</h1>
-    <v-data-table
-    v-model="selected"
-    show-select
-    :headers="headers"
-    :items="desserts"
-    :search="search"
-    item-key="name"
-    sort-by="calories"
-    class="elevation-1"
-  >
-    <template v-slot:top>
-      <v-toolbar
-        flat
-      >
-        <v-text-field
-        v-model="search"
-        append-icon="mdi-magnify"
-        label="Search"
-        single-line
-        hide-details
-      ></v-text-field>
-       
-        <v-divider
-          class="mx-4"
-          dark
-          vertical
-        ></v-divider>
-        <v-spacer></v-spacer>
+    <v-data-table v-model="selected" show-select :headers="headers" :items="clientes" :search="search" item-key="cuit" sort-by="nombre" class="elevation-1">
+        <template v-slot:top>
+            <v-toolbar flat>
+                <v-text-field v-model="search" append-icon="mdi-magnify" label="Búsqueda" single-line hide-details></v-text-field>
 
-        <v-btn
-            color="primary"
-            dark
-            class="mb-2"
-            v-bind="attrs"
-            v-on="on"
-            @click="editItem(selected[0])"
-          >
-            <v-icon>mdi-pencil</v-icon>
-          </v-btn>
+                <v-divider class="mx-4" dark vertical></v-divider>
+                <v-spacer></v-spacer>
 
-          <v-btn
-            color="error"
-            dark
-            class="mb-2"
-            v-bind="attrs"
-            v-on="on"
-            @click="deleteItem(selected)"
-          >
-            <v-icon>mdi-delete</v-icon>
-          </v-btn>
+                <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on" @click="editItem(selected[0])">
+                    <v-icon>mdi-pencil</v-icon>
+                </v-btn>
 
-          <v-dialog v-model="dialog" max-width="500px">
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn color="success" dark class="mb-2" v-bind="attrs" v-on="on">
-                <v-icon>mdi-plus</v-icon>
-              </v-btn>
-          </template>
-          <v-card>
-            <v-card-title>
-              <span class="headline">{{ formTitle }}</span>
-            </v-card-title>
+                <v-btn color="error" dark class="mb-2" v-bind="attrs" v-on="on" @click="deleteItem(selected)">
+                    <v-icon>mdi-delete</v-icon>
+                </v-btn>
 
-            <v-card-text>
-              <v-container>
-                <v-row>
-                  <v-col
-                    cols="12"
-                    sm="6"
-                    md="4"
-                  >
-                    <v-text-field
-                      v-model="editedItem.name"
-                      label="Dessert name"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col
-                    cols="12"
-                    sm="6"
-                    md="4"
-                  >
-                    <v-text-field
-                      v-model="editedItem.calories"
-                      label="Calories"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col
-                    cols="12"
-                    sm="6"
-                    md="4"
-                  >
-                    <v-text-field
-                      v-model="editedItem.fat"
-                      label="Fat (g)"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col
-                    cols="12"
-                    sm="6"
-                    md="4"
-                  >
-                    <v-text-field
-                      v-model="editedItem.carbs"
-                      label="Carbs (g)"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col
-                    cols="12"
-                    sm="6"
-                    md="4"
-                  >
-                    <v-text-field
-                      v-model="editedItem.protein"
-                      label="Protein (g)"
-                    ></v-text-field>
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-card-text>
+                <v-dialog v-model="dialog" max-width="500px">
+                    <template v-slot:activator="{ on, attrs }">
+                        <v-btn color="success" dark class="mb-2" v-bind="attrs" v-on="on">
+                            <v-icon>mdi-plus</v-icon>
+                        </v-btn>
+                    </template>
+                    <v-card>
+                        <v-card-title>
+                            <span class="headline">{{ formTitle }}</span>
+                        </v-card-title>
 
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn
-                color="blue darken-1"
-                text
-                @click="close"
-              >
-                Cancel
-              </v-btn>
-              <v-btn
-                color="blue darken-1"
-                text
-                @click="save"
-              >
-                Save
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-        <v-dialog v-model="dialogDelete" max-width="500px">
-          <v-card>
-            <v-card-title class="headline">Are you sure you want to delete this item?</v-card-title>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
-              <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
-              <v-spacer></v-spacer>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-      </v-toolbar>
-    </template>
-    <template v-slot:no-data>
-      <v-btn
-        color="primary"
-        @click="initialize"
-      >
-        Reset
-      </v-btn>
-    </template>
-  </v-data-table>
-  </v-container>
+                        <v-card-text>
+                            <v-container>
+                                <v-row>
+                                    <v-col cols="12" sm="6" md="4">
+                                        <v-text-field v-model="editedItem.nombre" label="Nombre y Apellido"></v-text-field>
+                                    </v-col>
+                                    <v-col cols="12" sm="6" md="4">
+                                        <v-text-field v-model="editedItem.cuit" label="CUIT"></v-text-field>
+                                    </v-col>
+                                    <v-col cols="12" sm="6" md="4">
+                                        <v-text-field v-model="editedItem.dni" label="DNI"></v-text-field>
+                                    </v-col>
+                                    <v-col cols="12" sm="6" md="4">
+                                        <v-text-field v-model="editedItem.tel" label="Teléfono"></v-text-field>
+                                    </v-col>
+                                    <v-col cols="12" sm="6" md="4">
+                                        <v-text-field v-model="editedItem.email" label="Email"></v-text-field>
+                                    </v-col>
+
+                                </v-row>
+                            </v-container>
+                        </v-card-text>
+
+                        <v-card-actions>
+                            <v-spacer></v-spacer>
+                            <v-btn color="blue darken-1" text @click="close">
+                                Cancel
+                            </v-btn>
+                            <v-btn color="blue darken-1" text @click="save">
+                                Save
+                            </v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-dialog>
+                <v-dialog v-model="dialogDelete" max-width="500px">
+                    <v-card>
+                        <v-card-title class="headline">Are you sure you want to delete this item?</v-card-title>
+                        <v-card-actions>
+                            <v-spacer></v-spacer>
+                            <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
+                            <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+                            <v-spacer></v-spacer>
+                        </v-card-actions>
+                    </v-card>
+                </v-dialog>
+            </v-toolbar>
+        </template>
+        <template v-slot:no-data>
+            <v-btn color="primary" @click="initialize">
+                Reset
+            </v-btn>
+        </template>
+    </v-data-table>
+      <v-snackbar v-model="snackbar">
+        {{ mensaje }}
+
+        <template v-slot:action="{ attrs }">
+            <v-btn color="pink" text v-bind="attrs" @click="snackbar = false">
+                Aceptar
+            </v-btn>
+        </template>
+    </v-snackbar>
+</v-container>
 </template>
 
 <script>
 export default {
     data: () => ({
-      selected: [],
-      search: '',
-      dialog: false,
-      dialogDelete: false,
-      headers: [
-        {
-          text: 'Dessert (100g serving)',
-          align: 'start',
-          sortable: false,
-          value: 'name',
+        selected: [],
+        search: '',
+        snackbar: false,
+        mensaje: "",
+        dialog: false,
+        dialogDelete: false,
+        headers: [{
+                text: 'Nombre y Apellido',
+                value: 'nombre',
+                align: 'start',
+                sortable: false,
+            },
+            {
+                text: 'DNI',
+                value: 'dni'
+            },
+            {
+                text: 'CUIT',
+                value: 'cuit'
+            },
+            {
+                text: 'Email',
+                value: 'email'
+            },
+            {
+                text: 'Teléfono',
+                value: 'tel'
+            },
+        ],
+        clientes: [],
+        editedIndex: -1,
+        attrs: '',
+        on: '',
+        editedItem: {
+            email: '',
+            nombre: '',
+            cuit: '',
+            dni: '',
+            tel: '',
         },
-        { text: 'Calories', value: 'calories' },
-        { text: 'Fat (g)', value: 'fat' },
-        { text: 'Carbs (g)', value: 'carbs' },
-        { text: 'Protein (g)', value: 'protein' },
-      ],
-      desserts: [],
-      editedIndex: -1,
-      attrs: '',
-      on: '',
-      editedItem: {
-        name: '',
-        calories: 0,
-        fat: 0,
-        carbs: 0,
-        protein: 0,
-      },
-      defaultItem: {
-        name: '',
-        calories: 0,
-        fat: 0,
-        carbs: 0,
-        protein: 0,
-      },
+        defaultItem: {
+            email: '',
+            nombre: '',
+            cuit: '',
+            dni: '',
+            tel: '',
+        },
     }),
 
     computed: {
-      formTitle () {
-        return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
-      },
+        formTitle() {
+            return this.editedIndex === -1 ? 'Nuevo Cliente' : 'Editar Cliente'
+        },
     },
 
     watch: {
-      dialog (val) {
-        val || this.close()
-      },
-      dialogDelete (val) {
-        val || this.closeDelete()
-      },
+        dialog(val) {
+            val || this.close()
+        },
+        dialogDelete(val) {
+            val || this.closeDelete()
+        },
     },
 
-    created () {
-      this.initialize()
+    created() {
+        this.initialize()
     },
 
     methods: {
-      initialize () {
-        this.desserts = [
-          {
-            name: 'Frozen Yogurt',
-            calories: 159,
-            fat: 6.0,
-            carbs: 24,
-            protein: 4.0,
-          },
-          {
-            name: 'Ice cream sandwich',
-            calories: 237,
-            fat: 9.0,
-            carbs: 37,
-            protein: 4.3,
-          },
-          {
-            name: 'Eclair',
-            calories: 262,
-            fat: 16.0,
-            carbs: 23,
-            protein: 6.0,
-          },
-          {
-            name: 'Cupcake',
-            calories: 305,
-            fat: 3.7,
-            carbs: 67,
-            protein: 4.3,
-          },
-          {
-            name: 'Gingerbread',
-            calories: 356,
-            fat: 16.0,
-            carbs: 49,
-            protein: 3.9,
-          },
-          {
-            name: 'Jelly bean',
-            calories: 375,
-            fat: 0.0,
-            carbs: 94,
-            protein: 0.0,
-          },
-          {
-            name: 'Lollipop',
-            calories: 392,
-            fat: 0.2,
-            carbs: 98,
-            protein: 0,
-          },
-          {
-            name: 'Honeycomb',
-            calories: 408,
-            fat: 3.2,
-            carbs: 87,
-            protein: 6.5,
-          },
-          {
-            name: 'Donut',
-            calories: 452,
-            fat: 25.0,
-            carbs: 51,
-            protein: 4.9,
-          },
-          {
-            name: 'KitKat',
-            calories: 518,
-            fat: 26.0,
-            carbs: 65,
-            protein: 7,
-          },
-        ]
-      },
+        initialize() {
+            this.clientes = [{
+                    email: 'Frozen Yogurt',
+                    nombre: '159',
+                    cuit: '6.0',
+                    dni: '24',
+                    tel: '4.0',
+                },
+                {
+                    email: 'Ice cream sandwich',
+                    nombre: '237',
+                    cuit: '9.0',
+                    dni: '37',
+                    tel: '4.3',
+                },
+                {
+                    email: 'Eclair',
+                    nombre: '262',
+                    cuit: '16.0',
+                    dni: '23',
+                    tel: '6.0',
+                },
+                {
+                    email: 'Cupcake',
+                    nombre: '305',
+                    cuit: '3.7',
+                    dni: '67',
+                    tel: '4.3',
+                },
+                {
+                    email: 'Gingerbread',
+                    nombre: '356',
+                    cuit: '100',
+                    dni: '49',
+                    tel: '3.9',
+                },
+                {
+                    email: 'Jelly bean',
+                    nombre: '375',
+                    cuit: '0.0',
+                    dni: '94',
+                    tel: '0.0',
+                },
 
-      editItem (item) {
-        if(this.selected.length === 1){
-          this.editedIndex = this.desserts.indexOf(item)
-          this.editedItem = Object.assign({}, item)
-          this.dialog = true
-        }
-      },
+            ]
+        },
+        haySeleccionado() {
+            return this.selected.length>0;
+        },
+        mensajeNoSelecciono() {
+            if (!this.haySeleccionado()) {
+                this.snackbar = true
+                this.mensaje = "No ha seleccionado ningun elemento!"
+                return true;
+            }
+            return false;
+        },
 
-      deleteItem (items) {
-          this.editedIndex = this.desserts.indexOf(items)
-          this.editedItem = Object.assign({}, items)
-        
-        this.dialogDelete = true
-      },
+        editItem(item) {
+            if (!this.mensajeNoSelecciono() & this.selected.length === 1) {
+                this.editedIndex = this.clientes.indexOf(item)
+                this.editedItem = Object.assign({}, item)
+                this.dialog = true
+            }
+        },
 
-      deleteItemConfirm () {
-        this.selected.forEach(item => {
-          this.desserts.splice(this.desserts.indexOf(item), 1);
-        });
-        this.closeDelete()
-      },
+        deleteItem(items) {
+          if(!this.mensajeNoSelecciono()){
+            this.editedIndex = this.clientes.indexOf(items)
+            this.editedItem = Object.assign({}, items)
 
-      close () {
-        this.dialog = false
-        this.$nextTick(() => {
-          this.editedItem = Object.assign({}, this.defaultItem)
-          this.editedIndex = -1
-        })
-      },
+            this.dialogDelete = true
+          }
+        },
 
-      closeDelete () {
-        this.dialogDelete = false
-        this.$nextTick(() => {
-          this.editedItem = Object.assign({}, this.defaultItem)
-          this.editedIndex = -1
-        })
-      },
+        deleteItemConfirm() {
+            this.selected.forEach(item => {
+                this.clientes.splice(this.clientes.indexOf(item), 1);
+            });
+            this.closeDelete()
+        },
 
-      save () {
-        if (this.editedIndex > -1) {
-          Object.assign(this.desserts[this.editedIndex], this.editedItem)
-        } else {
-          this.desserts.unshift(this.editedItem)
-        }
-        this.close()
-      },
+        close() {
+            this.dialog = false
+            this.selected= []
+            this.$nextTick(() => {
+                this.editedItem = Object.assign({}, this.defaultItem)
+                this.editedIndex = -1
+            })
+        },
+
+        closeDelete() {
+            this.dialogDelete = false
+            this.$nextTick(() => {
+                this.editedItem = Object.assign({}, this.defaultItem)
+                this.editedIndex = -1
+            })
+        },
+
+        save() {
+            if (this.editedIndex > -1) {
+                Object.assign(this.clientes[this.editedIndex], this.editedItem)
+            } else {
+                this.clientes.unshift(this.editedItem)
+            }
+            this.close()
+            this.selected= []
+        },
     },
-  }
+}
 </script>
 
 <style>
-.titulo{
-  text-align: center;
-  margin: 20px;
+.titulo {
+    text-align: center;
+    margin: 20px;
 }
-.mb-2{
-  margin-left: 3px;
-  margin-right: 3px;
+
+.mb-2 {
+    margin-left: 3px;
+    margin-right: 3px;
 }
 </style>
