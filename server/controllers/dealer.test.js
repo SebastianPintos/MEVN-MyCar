72ff5b5ed6cf3b834c470b3661b82a3b0c704f70
FrancoBarberis <francobarberissic3@gmail.com>
2020-11-09 20:49:31 -0300

    Revert "Revert "Prototipo de tests""
    This reverts commit 712dccc93c5f35237c6f65f5a9d2784edce79994.

---
+++ server/controllers/dealer.test.js
@@ -0,0 +1,15 @@
+const dealer = require ("./dealer");
+
+const Dealer = new Dealer {(
+    Name: "José Martínez",
+    Phone: "1140288399",
+    Email: "jmartinez@gmail.com",
+    Address: (Street: "El Callao",Number: "1883",City: "Grand Bourg"),
+    Status: "ACTIVE"
+)
+};
+
+test ("Properly creates a client", () =>{
+    expect (client.index(Cliente,res))
+    .toBe(client.create(Cliente,res))
+})
