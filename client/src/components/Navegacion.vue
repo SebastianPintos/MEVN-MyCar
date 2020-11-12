3e1b1a97d853b87775efbd62e092e8fbb5d3ca47
solpierozzi <solpierozzi@hotmail.com>
2020-11-12 01:04:26 -0300

    Agregada view proveedores

--- client/src/components/Navegacion.vue
+++ client/src/components/Navegacion.vue
@@ -56,6 +56,13 @@
             <v-list-item-title>Clientes</v-list-item-title>
           </v-list-item>
 
+           <v-list-item :to="'/proveedores'">
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Proveedores</v-list-item-title>
+          </v-list-item>
+
           <v-list-item :to="'/vehiculos'">
             <v-list-item-icon>
               <v-icon>mdi-car</v-icon>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -56,6 +56,16 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "clientes" */ "../views/Clientes.vue"),
     meta: { title: 'Clientes' }
+  },
+  {
+    path: "/proveedores",
+    name: "Proveedores",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "proveedores" */ "../views/Proveedores.vue"),
+    meta: { title: 'Proveedores' }
   }
 ];
 
