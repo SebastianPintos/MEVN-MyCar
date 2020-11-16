849be26cbac5576b92edf3325c3bffea48b5dd95
solpierozzi <solpierozzi@hotmail.com>
2020-11-15 21:24:28 -0300

    Crea stock a partir de archivo txt y verifica stock

--- server/models/productStock.js
+++ server/models/productStock.js
@@ -3,9 +3,16 @@ var Schema = mongoose.Schema;
 
 var ProductStockSchema = new Schema({
   Code: {type: String,required: true},
-  BatchNum: {type: String,required: true},
-  State: {type: String,required: true},
+  BatchNum: {type: String},
+  Reserved: {type: Number},
+  Available: {type: Number},
+  OutOfService: {type: Number},
+  TotalOrdered: {type: Number},
+  Expiration: {type: Date},
+  OrderDate: {type: Date},
+  ArrivalDate: {type: Date},
   Product: {type: Schema.Types.ObjectId,required: true,ref: 'Product'},
+  Status: {type: String, enum: ['ACTIVE', 'INACTIVE'], required: true},
 });
 
 var ProductStock = mongoose.model("ProductStock", ProductStockSchema);
