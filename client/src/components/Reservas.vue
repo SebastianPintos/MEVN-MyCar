8922ae5ec9088c430896c1f16fdef12cb7b54fac
solpierozzi <solpierozzi@hotmail.com>
2020-11-20 13:16:34 -0300

    Cambiado look and feel reservas

--- client/src/components/Reservas.vue
+++ client/src/components/Reservas.vue
@@ -3,27 +3,54 @@
 
     <template>
         <br>
+
+        <v-toolbar flat>
+            <v-flex class="text-right">
+                <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on">
+                    <v-icon>mdi-pencil</v-icon>
+                </v-btn>
+                <v-btn color="error" dark class="mb-2" v-bind="attrs" v-on="on">
+                    <v-icon>mdi-delete</v-icon>
+                </v-btn>
+                <v-btn color="success" dark class="mb-2" v-bind="attrs" v-on="on" @click="nuevoTurno=true">
+                    <v-icon>mdi-plus</v-icon>
+                </v-btn>
+            </v-flex>
+        </v-toolbar>
+    </template>
+
+    <v-dialog v-model="nuevoTurno" max-width="400px">
         <v-card>
-        
-            <v-row>
-                <v-col cols="12" md="6">
-                    <v-menu ref="menu" v-model="menu" :close-on-content-click="false" transition="scale-transition" offset-y min-width="290px">
-                        <template v-slot:activator="{ on, attrs }">
-                            <v-text-field v-model="date" label="Día" prepend-icon="mdi-calendar" readonly v-bind="attrs" v-on="on"></v-text-field>
-                        </template>
-                        <v-date-picker ref="picker" v-model="date" :max="new Date().toISOString().substr(0, 10)" min="1950-01-01" @change="save"></v-date-picker>
-                    </v-menu>
-                </v-col>
-                <v-col cols="12" md="6">
-                    <v-select label="Horario" :items="horarios"></v-select>
-                </v-col>
-            </v-row>
+            <v-card-title>
+                <h1>Día y Horario</h1>
+            </v-card-title>
+            <v-card-text>
+                <v-row>
+                    <v-col cols="12" md="6">
+                        <v-menu ref="menu" v-model="menu" :close-on-content-click="false" transition="scale-transition" offset-y min-width="290px">
+                            <template v-slot:activator="{ on, attrs }">
+                                <v-text-field v-model="date" label="Día" prepend-icon="mdi-calendar" readonly v-bind="attrs" v-on="on"></v-text-field>
+                            </template>
+                            <v-date-picker ref="picker" v-model="date" :max="new Date().toISOString().substr(0, 10)" min="1950-01-01" @change="save"></v-date-picker>
+                        </v-menu>
+                    </v-col>
+                    <v-col cols="12" md="6">
+                        <v-select label="Horario" :items="horarios"></v-select>
+                    </v-col>
+                </v-row>
+            </v-card-text>
             <v-card-actions>
-            <v-btn class="warning" block>Reservar</v-btn>
+                <v-flex class="text-right">
+                    <v-btn color="info" dark class="mb-2" v-bind="attrs" v-on="on" @click="nuevoTurno=false">
+                        <v-icon>mdi-cancel</v-icon>
+                    </v-btn>
+                    <v-btn color="info" dark class="mb-2" v-bind="attrs" v-on="on">
+                        <v-icon>mdi-check</v-icon>
+                    </v-btn>
+                </v-flex>
             </v-card-actions>
         </v-card>
-
-    </template>
+    </v-dialog>
 
     <v-sheet tile height="54" class="d-flex">
         <v-btn icon class="ma-2" @click="$refs.calendar.prev()">
@@ -44,19 +71,18 @@
 <script>
 import axios from "axios"
 export default {
-    created(){
+    created() {
 
         let sucursal = JSON.parse(localStorage.getItem("sucursal"));
         let id = sucursal.id;
-       console.log("SUCURSAL: "+JSON.stringify(sucursal)+" id "+id);
-       if(sucursal!= null){
-           this.getReservas(sucursal.id);
-       }
-        let direccionActual = String(location.href);
-        if(direccionActual.includes("/turno")){
-            console.log("Calendario!");
+        console.log("SUCURSAL: " + JSON.stringify(sucursal) + " id " + id);
+        if (sucursal != null) {
+            this.getReservas(sucursal.id);
         }
-        else{
+        let direccionActual = String(location.href);
+        if (direccionActual.includes("/turno")) {
+            console.log("Calendario!");
+        } else {
             console.log("Reserva");
         }
     },
@@ -65,6 +91,7 @@ export default {
         horarios: ['08:00', '08:30', '9:00', '9:30', '10:00', '10:30'],
         attrs: '',
         on: '',
+        nuevoTurno: false,
         date: null,
         menu: false,
         type: 'month',
@@ -108,24 +135,24 @@ export default {
                 this.type = 'week'
             }
         },
-         async getReservas(sucursal) {
+        async getReservas(sucursal) {
             await axios.get('http://localhost:8081/reservation')
                 .then(res => {
-                   this.reservas = res.data.reservation.filter(reserva => reserva.Status === "ACTIVE" & reserva.BranchOffice == sucursal);
-                   console.log(JSON.stringify(this.reservas));
+                    this.reservas = res.data.reservation.filter(reserva => reserva.Status === "ACTIVE" & reserva.BranchOffice == sucursal);
+                    console.log(JSON.stringify(this.reservas));
                     this.getEvents();
                 });
         },
         getEvents() {
             const events = []
-           for (let i = 0; i <this.reservas.length; i++) {
-               let desde = new Date(this.reservas[i].AppointmentTime);
-               let duracion = this.reservas[i].Duration;
-               let hasta = new Date(desde.getTime() + duracion * 60000 );
-                let sMinutesDesde = desde.getMinutes() == 0 ? "00": String(desde.getMinutes());
-                let sMinutesHasta = hasta.getMinutes() == 0 ? "00": String(hasta.getMinutes());
+            for (let i = 0; i < this.reservas.length; i++) {
+                let desde = new Date(this.reservas[i].AppointmentTime);
+                let duracion = this.reservas[i].Duration;
+                let hasta = new Date(desde.getTime() + duracion * 60000);
+                let sMinutesDesde = desde.getMinutes() == 0 ? "00" : String(desde.getMinutes());
+                let sMinutesHasta = hasta.getMinutes() == 0 ? "00" : String(hasta.getMinutes());
                 events.push({
-                    name: desde.getHours()+":"+sMinutesDesde+"-"+hasta.getHours()+":"+sMinutesHasta+" Reservado",
+                    name: desde.getHours() + ":" + sMinutesDesde + "-" + hasta.getHours() + ":" + sMinutesHasta + " Reservado",
                     start: desde,
                     end: hasta,
                     color: this.colors[0],
