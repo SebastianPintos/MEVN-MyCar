ed3719e82f6e37b5ac7bb67e506eb64264c8bd1c
snramirez <54188400+snramirez@users.noreply.github.com>
2020-12-11 14:59:04 -0300

    checkear minimos

--- server/lib/helperStock.js
+++ server/lib/helperStock.js
@@ -12,25 +12,53 @@ Busca en los controles minimos si encuentra minimo para ese producto
 Si encuentra un minimo y la cantidad de disponibles del producto es menor, genera orden de compra*/
 helperStock.checkMin = async (repuesto) => {
    let productQuantity = 0;
+   let product;
 
    await ProductStock.find({Product: repuesto.Product, Status: 'ACTIVE'}, (err, products) => {
        if(err) {console.log(err)}
        else{
+           console.log(products);
+           product = products[0];
            for(y = 0; y < products.length; y++){
                 productQuantity += products[y].Available;
            }
        }
-    });
+    }).populate('Product');
+
+    console.log('despues de buscar los productos', product);
+    console.log('cantidad disponible ', productQuantity);
+    
    
     await ProductControl.find(async (err, productControl) => {   
-    for(let i=0; i< productControl.length; i++){
-           if(repuesto.Product == productControl[i].Product && productQuantity < productControl[i].Min){
+        console.log(productControl);
+        for(let i=0; i< productControl.length; i++){
+        console.log('producto recibido ', repuesto.Product);
+        console.log('producto controll ', productControl[i].Product);
+        console.log('cantidad disponible ', productQuantity);
+        console.log('min  ', productControl[i].Min);
+
+           if(repuesto.Product.toString() === productControl[i].Product.toString() && productQuantity < productControl[i].Min){
                console.log("GENERAR ORDEN");
-               /* let purchaseOrder =  PurchaseOrder({
+               let purchaseOrder =  new PurchaseOrder({
                    OrderDate: new Date(),
-                   //Price:
-                   Product:[]
-               }) */
+                   Price: product.Product.SalePrice,
+                   Product:[{
+                       ProductID: product._id,
+                       BatchNum: product.BatchNum,
+                       Expiration: product.Expiration,
+                       TotalOrder: '1',
+                       Price: product.Product.SalePrice
+                   }],
+                   Dealer: product.Product.Dealer,
+                   BranchOffice: product.BranchOffice,
+                   Status: 'ACTIVE'
+               })
+               console.log(purchaseOrder);
+
+               purchaseOrder.save((err) => {
+                if (err) { console.log(err) }
+                else {console.log('guardo bien')};
+            });
            }
        }
    });
@@ -46,7 +74,7 @@ helperStock.checkMinCreation = async (controlStock) => {
         
         let productQuantity = 0;
         for(y = 0; y < productStock.length; y++){
-            productQuantity += products[y].Available;
+            productQuantity +=  products[y].Available;
        }
      
         if(productQuantity < controlStock.Min){
