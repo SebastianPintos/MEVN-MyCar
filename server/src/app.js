7afd7d743028da6830fe5e19c94cdb89d7fe777b
snramirez <54188400+snramirez@users.noreply.github.com>
2020-12-18 17:03:10 -0300

    cambios en la frecuencia de schedule

--- server/src/app.js
+++ server/src/app.js
@@ -21,8 +21,8 @@ var db = mongodb_conn_module.connect();
 └───────────────────────── second (0 - 59, OPTIONAL)*/
 
 //Cada hora, de lunes a viernes 0 */1 * * 1-5
-schedule.scheduleJob('0 */1 * * 1-5', cronJob.EmailRemainder1);
-schedule.scheduleJob('0 */1 * * 1-5', cronJob.EmailRemainder2);
+schedule.scheduleJob('0 */5 * * 1-5', cronJob.EmailRemainder1);
+schedule.scheduleJob('0 */5 * * 1-5', cronJob.EmailRemainder2);
 
 
 //Starting Server
